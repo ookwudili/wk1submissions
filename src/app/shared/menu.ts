e7c6fd21871a174ae9cd9f7393b4086cf70b9950
ookwudili <ookwudili@live.co.uk>
2020-06-28 13:03:06 -0400

    1 file added

---
+++ src/app/shared/menu.ts
@@ -0,0 +1,10 @@
+export class Menu {
+    id: string;
+    name: string;
+    image: string;
+    category: string;
+    featured: boolean;
+    label: string;
+    price: string;
+    description: string;
+}
